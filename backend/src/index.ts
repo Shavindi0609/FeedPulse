8adb34265bc4ae49bccbb59053939f5a2dfe7705
#Shanu0609@ <shavindialoka69@gmail.com>
2026-04-02 10:01:24 +0530

    Initial commit: fullstack structure with backend and frontend

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('FeedPulse API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on http://localhost:${PORT}`);
+});
